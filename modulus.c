f0299ae2cbf12dd7e9573199411ea927c6c180e4
raghulai <raghulai01@gmail.com>
2024-01-04 12:18:39 +0530

    task2

---
+++ modulus.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+     int a;
+    printf("enter the value A :");
+    scanf("%d",&a);
+    if (a%2==0)
+    {
+        printf("the value is even number");
+    }
+    else   
+     {
+        printf("the value is odd number");
+    }
+    return 0;
+}
